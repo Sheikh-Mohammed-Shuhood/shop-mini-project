77e43c5725b1459f0a8c643a3c2bd0278aa6df90
Shrii939 <shridhars145@gmail.com>
2023-01-17 15:44:34 +0530

    delete page

--- Delete.php
+++ Delete.php
@@ -12,13 +12,13 @@ $conn = mysqli_connect('localhost', 'root', '', 'shop') or die("Unable to connec
     <title>search</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
     <link rel="stylesheet" href="../css/product.css" />
-   
+
     <link rel="stylesheet" href="../search.css" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<div>
+    <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
@@ -51,15 +51,15 @@ $conn = mysqli_connect('localhost', 'root', '', 'shop') or die("Unable to connec
                         <button class="btn btn-outline-success me-2" type="submit">
                             delete
                         </button>
-                       
+
                     </form>
-                    
+
                 </div>
             </div>
         </nav>
     </div>
-    
-    <div class="container-sm col-sm-5 mt-md-3" >
+
+    <div class="container-sm col-sm-5 mt-md-3">
 
         <form class="d-flex" role="search" method="post" id="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchstr" id="search-input" />
@@ -67,10 +67,35 @@ $conn = mysqli_connect('localhost', 'root', '', 'shop') or die("Unable to connec
 
         </form>
     </div>
-    
+
 
     <?php
-        echo "<div class='container table table-bordered table-responsive-sm mt-5' id='originalTable'>";
+    echo "<div class='container table table-bordered table-responsive-sm mt-5' id='originalTable'>";
+    echo " <table class='table'>";
+    echo "<thead>";
+    echo "  <tr>";
+    echo "    <th>Product ID</th>";
+    echo "    <th>Product</th>";
+    echo "    <th>Price</th>";
+    echo "    <th>Stock</th>";
+    echo "  </tr>";
+    echo " </thead>";
+    echo "<tbody>";
+    // Fetch data from the database
+    $sql = "SELECT * FROM product";
+    $result = $conn->query($sql);
+    // Loop through each row of data
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["PID"] . "</td>";
+        echo "<td>" . $row["PRODNAME"] . "</td>";
+        echo "<td>" . $row["PRODCOST"] . "</td>";
+        echo "<td>" . $row["INSTOCK"] . "</td>";
+        echo "</tr>";
+    }
+
+    if (isset($_POST["search"])) {
+        echo "<div class='container table table-bordered table-responsive-sm mt-3' id='secondTable'>";
         echo " <table class='table'>";
         echo "<thead>";
         echo "  <tr>";
@@ -82,44 +107,23 @@ $conn = mysqli_connect('localhost', 'root', '', 'shop') or die("Unable to connec
         echo " </thead>";
         echo "<tbody>";
         // Fetch data from the database
-        $sql = "SELECT * FROM product";
-        $result = $conn->query($sql);
-        // Loop through each row of data
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["pid"] . "</td>";
-            echo "<td>" . $row["PRODNAME"] . "</td>";
-            echo "<td>" . $row["PRODCOST"] . "</td>";
-            echo "<td>" . $row["INSTOCK"] . "</td>";
-            echo "</tr>";
-        }
+        $sql = "DELETE FROM product WHERE PID= '$_POST[searchstr]'";
 
-        if(isset($_POST["search"])){
-            echo "<div class='container table table-bordered table-responsive-sm mt-3' id='secondTable'>";
-            echo " <table class='table'>";
-            echo "<thead>";
-            echo "  <tr>";
-            echo "    <th>Product ID</th>";
-            echo "    <th>Product</th>";
-            echo "    <th>Price</th>";
-            echo "    <th>Stock</th>";
-            echo "  </tr>";
-            echo " </thead>";
-            echo "<tbody>";
-            // Fetch data from the database
-            $sql = "DELETE FROM product WHERE pid= '$_POST[searchstr]'";
-            $result = $conn->query($sql);
+        $ar = mysqli_affected_rows($conn);
+        if ( $ar > 0) {
+
             // Loop through each row of data
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
-                echo "<td>" . $row["pid"] . "</td>";
+                echo "<td>" . $row["PID"] . "</td>";
                 echo "<td>" . $row["PRODNAME"] . "</td>";
                 echo "<td>" . $row["PRODCOST"] . "</td>";
                 echo "<td>" . $row["INSTOCK"] . "</td>";
                 echo "</tr>";
             }
-            
         }
+    }
+
     ?>
     </tbody>
     </table>
@@ -128,4 +132,5 @@ $conn = mysqli_connect('localhost', 'root', '', 'shop') or die("Unable to connec
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
 </html>
